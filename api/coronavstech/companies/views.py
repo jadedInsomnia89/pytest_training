4b0ea637e49f8cb2fdb298b62669e2d18580143c
jadedInsomnia89 <jbcrumley1@gmail.com>
2021-11-16 17:44:28 -0600

    added email sending functionality (leaving out settings file for now because it contains sensitive data

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,6 +1,10 @@
 # from django.shortcuts import render
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from django.core.mail import send_mail
 
 from api.coronavstech.companies.models import Company
 from api.coronavstech.companies.serializers import CompanySerializer
@@ -10,3 +14,18 @@ class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all().order_by('-last_update')
     pagination_class = PageNumberPagination
+
+
+@api_view(http_method_names=['POST'])
+def send_company_email(request:Request) -> Response:
+    '''
+        sends email with request payload
+        sender: whosyerdady2007@gmail.com
+        receiver: whosyerdady2007@gmail.com
+    '''
+    send_mail(subject=request.data.get('subject'), message=request.data.get('message'), from_email='whosyerdady2007@gmail.com', recipient_list=['whosyerdady2007@gmail.com'])
+    return Response({
+        'status': 'success',
+        'info': 'email sent successfully'
+    },
+                    status=200)
--- api/coronavstech/coronavstech/urls.py
+++ api/coronavstech/coronavstech/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 from django.urls.conf import include
 
 from api.coronavstech.companies.urls import companies_router
+from api.coronavstech.companies.views import send_company_email
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(companies_router.urls))
+    path('', include(companies_router.urls)),
+    path('send-email', send_company_email)
 ]
