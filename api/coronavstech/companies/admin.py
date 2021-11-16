307f35275d4ad1c6123eda590d289f69511b85b1
jadedInsomnia89 <jbcrumley1@gmail.com>
2021-11-15 19:32:08 -0600

    Created CompanySerializer, CompanyView, routes, and added values to PYTHONPATH and changed relative imports to absolute imports

--- api/coronavstech/companies/admin.py
+++ api/coronavstech/companies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company
+from api.coronavstech.companies.models import Company
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
--- api/coronavstech/coronavstech/urls.py
+++ api/coronavstech/coronavstech/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
+from api.coronavstech.companies.urls import companies_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(companies_router.urls))
 ]
