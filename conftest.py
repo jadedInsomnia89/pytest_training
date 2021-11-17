b29a5bbf01a99f554bc5ec4f6cab98c9d4d1257c
jadedInsomnia89 <jbcrumley1@gmail.com>
2021-11-17 14:13:05 -0600

    created a track_performace decorator, test_performance, and dynamic fibonacci function as well as changed fixtures filename to conftest so the fixtures can be used anywhere in the project and added pytest env variables

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from datetime import datetime, timedelta
+from typing import Callable
+
+
+class PerformanceException(Exception):
+    def __init__(self, runtime: timedelta, limit: timedelta) -> None:
+        self.runtime = runtime
+        self.limit = limit
+
+    def __str__(self) -> str:
+        return f'Performance test failed.\nRuntime: {self.runtime.total_seconds()}\nLimit: {self.limit.total_seconds()}'
+
+
+@pytest.fixture
+def time_tracker():
+    start_time = datetime.now()
+    yield
+    end_time = datetime.now()
+    time_elapsed = end_time - start_time
+    print(f'\nRuntime: {time_elapsed.total_seconds()}')
+
+
+def track_performance(method: Callable, runtime_limit=timedelta(seconds=2)):
+    def run_function_and_validate_runtime(*args, **kwargs):
+        start_time = datetime.now()
+        result = method(*args, **kwargs)
+        end_time = datetime.now()
+        runtime = end_time - start_time
+        print(f'\nRuntime: {runtime.total_seconds()}')
+
+        if runtime > runtime_limit:
+            raise PerformanceException(runtime=runtime, limit=runtime_limit)
+
+        return result
+
+    return run_function_and_validate_runtime
