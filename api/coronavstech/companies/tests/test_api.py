6275dbe19bdf64646435e565c015464f35098666
jadedInsomnia89 <jbcrumley1@gmail.com>
2021-11-23 11:24:41 -0600

    added pytest-xdist and formatted files with yapf

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 from api.coronavstech.companies.models import Company
 
-companies_url = '/companies/' # reverse('companies-list')
+companies_url = '/companies/'  # reverse('companies-list')
 logger = logging.getLogger('CORONA_LOGS')
 pytestmark = pytest.mark.django_db
 
